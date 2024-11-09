feb9dcb63a80ccf4b616c8d3e4390d52c46641e1
HelySuarez <hsuarez007@gmail.com>
2024-11-08 20:23:14 -0500

    se realiza frontend de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
-    provideClientHydration()
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/services/empleado.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Empleado } from '../models/empleado';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpleadoService {
+
+  selectedEmpleado: Empleado;
+  empleado: Empleado[];
+  readonly URL_API = "https://backend-one-rose-12.vercel.app/api/empleados";
+
+  constructor(private http:HttpClient) {
+    this.selectedEmpleado = new Empleado();
+    this.empleado=[];
+   }
+
+   getEmpleados(){
+    return this.http.get(this.URL_API);
+   }
+   postEmpleado(Empleado:Empleado){
+   return this.http.post(this.URL_API, Empleado);
+   }
+
+   putEmpleado(Empleado:Empleado){
+    return this.http.put(this.URL_API + `/${Empleado._id}`, Empleado);
+   }
+
+   deleteEmpleado(_id:string){
+    return this.http.delete(this.URL_API + `/${_id}`,)
+   }
+    
+      
+}
+
